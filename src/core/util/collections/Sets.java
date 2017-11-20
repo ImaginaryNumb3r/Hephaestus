6782b969621ca82ea4f2dc6b8b3023b1b4a01a99
ImaginaryNumb3r <Patrick@Plieschnegger.com>
2017-11-20 23:36:26 +0100

    fixed sets

--- src/core/util/collections/Sets.java
+++ src/core/util/collections/Sets.java
@@ -4,6 +4,7 @@ import core.util.collections.iteration.Iterables;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Patrick
@@ -11,21 +12,21 @@ import java.util.HashSet;
  */
 public final class Sets {
 
-    public <T> HashSet from(T... items){
+    public static <T> Set<T> from(T... items){
         return from(Iterables.from(items), items.length);
     }
 
-    public <T> HashSet from(Iterable<T> iterable){
+    public static <T> Set<T> from(Iterable<T> iterable){
         return from(iterable, 16); // Default Capacity of HashMap, as of JDK 9.
     }
 
-    public <T> HashSet from(Iterable<T> iterable, int size){
+    public static <T> Set<T> from(Iterable<T> iterable, int size){
         HashSet<T> set = new HashSet<>(size);
         iterable.forEach(set::add);
         return set;
     }
 
-    public <T> HashSet from(Collection<T> collection){
+    public static <T> Set<T> from(Collection<T> collection){
         HashSet<T> set = new HashSet<>(collection.size());
         set.addAll(collection);
         return set;
